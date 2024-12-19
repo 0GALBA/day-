1fdef3d5caf778f8981cbff4dfa0c0286478417d
gabriel dubus <gabrielloupisoleil22@gmail.com>
2024-12-19 12:29:02 +0100

    3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{fs::File, io::{empty, Read}};
+use std::{fs::File, io::{Lines, Read}};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Block {
     Files(i32, i32),  // size, index
     Empty(i32),       // size
@@ -49,25 +49,43 @@ impl Disk {
         None
     }
 
-    fn get_last_filles(&self) -> Option<&Block> {
+    fn get_last_filles(&mut self) -> Option<(Block,usize)> {
         // Renvoyer le dernier block de type Files
-        for block in self.blocks.iter().rev() {
+        for (i, block,) in self.blocks.iter().rev().enumerate() {
             if let Block::Files(..) = block {
-                return Some(block);
+                return Some((block.clone(), self.blocks.len() - i - 1));
             }
         }
         None
     }
 }
 
-fn reorganize_blocks(mut disk: Disk) {
-    for (pos, block) in disk.blocks.iter_mut().enumerate() {
-        if let Block::Empty(empty_size) = block {
-            if let Some(Block::Files(file_size, file_index)) = disk.get_last_filles() {
-                let num_to_copy = *empty_size as usize;
-                for i in 0..num_to_copy {
-                    if pos + i < disk.blocks.len() {
-                        disk.blocks[pos + i] = Block::Files(*file_size, *file_index);
+fn reorganize_blocks(disk: &mut Disk) {
+    for ref mut pos in 0..disk.blocks.len() {
+        let block = disk.blocks.get(*pos).unwrap();
+        if let Block::Empty(empty_size) = *block {
+            if let Some((b, last_pos)) = disk.get_last_filles() {
+                if last_pos < *pos {
+                    break;
+                }
+
+                if let Block::Files(file_size, file_index) = b {
+                    if file_size <= empty_size {
+                        let file = Block::Files(file_size, file_index);
+                        disk.blocks.remove(*pos);
+                        disk.blocks.insert(*pos, file);
+                        disk.blocks.remove(last_pos);
+                        let file = Block::Empty(empty_size - file_size);
+                        disk.blocks.insert(*pos + 1, file);
+                    } else if empty_size < file_size {
+                        let file = Block::Files(empty_size, file_index);
+                        disk.blocks.remove(*pos);
+                        disk.blocks.insert(*pos, file);
+                        *pos += 1;
+                        let file = Block::Files(file_size - empty_size, file_index);
+                        disk.blocks.remove(last_pos);
+                        disk.blocks.insert(last_pos, file);
+                        
                     }
                 }
             }
@@ -76,14 +94,15 @@ fn reorganize_blocks(mut disk: Disk) {
 }
 //faire une première passe ou on fais en sorte de ne rien supprimer
 //regarder le 1er empty et reconaitre sa longeur pour ajouter la meme longeure a la place du empty a partir du fichier suit a cela supprimer le empty et la longuerre du empty au fichier.
-//insert
+//insert        remouve 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = String::new();
     let mut f = File::open("data.csv")?;
     f.read_to_string(&mut buffer)?;
-    let loulou = Disk::new_from_string(buffer);
-    println!("{:?}", loulou);
+    let mut loulou = Disk::new_from_string(buffer);
+    reorganize_blocks(&mut loulou);
+    println!("{:?}", loulou.blocks);
 
     Ok(())
 }
